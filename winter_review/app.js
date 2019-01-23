2ef8c9b795b83fe55b7e3902499f2eea00903d05
Trinal0821 <find@A5-004030.localdomain>
2019-01-23 16:33:56 -0700

    first commit

---
+++ winter_review/app.js
@@ -0,0 +1,142 @@
+// alert
+alert('this is an alert')
+
+// multi line comment
+/*
+this 
+is 
+a 
+multi
+line 
+comment */
+
+// single line comment is //
+
+// math
+var number = 5 * 18
+
+// order of operations
+number = (8 * 12) + 18 / 2;
+
+// strings
+"this is a string of text"
+'String of text and numbers'
+
+//concatination (adding strings together)
+string1 = "Hello"
+string2 = "FIND"
+return string1 + string2 
+
+// = vs == vs ===
+// = single assignment or assigns a value
+// ==equal to value
+// === checks for equality and data type
+
+//integerws 
+1 5 10 -12
+
+// floats
+ decimal 
+ 1.2 6.8 -12.82
+
+ //booleans
+ true false / yes no/ 0 1
+
+ //arrays
+ [
+     item 1,
+     item 2,
+     item 3,
+     {name: 'name', complete: true}
+ ]
+
+ // hash/key-value pair 
+ { name: ' todo Item 1' complete: true}
+ // array of hashes
+ [
+     { item_ name: 'Eat', complete: false},
+     {item_name: 'code', complete: false},
+     {item_name: 'Repeat', complete: false}
+ ]
+
+// Use Case
+people = [
+    {name: 'jace', age 21, has_dogs: true},
+    { name ''}
+]
+
+ // function 
+ // is a set of precedures to keep your coe from repeating itself
+ function sayHello() {
+   console.log('Hello')
+ }
+ // calling a function
+ sayHello()
+
+ // function with params
+ function greetPerson(name) {
+   console.log('Hello, ' + name)
+ }
+ 
+ greetPerson('Jace') = "Hello, Jace"
+ greetperson('Jake') = "Hello, Jake"
+ greetperson('Henry') = "Hello, Henry"
+ greetperson('Racoon') = "Hello, Racoon"
+ greetperson('Goose') = "Hello, Goose"
+
+ //reassignment
+ var name ='Jace'
+ //name is equal to Jace
+ name = 'Jake'
+ //name is now equal to Jake
+
+ //console log
+ console.log(name)
+
+ //conditions
+ if (name === 'Jace') {
+   console.log('Jace is awesome')
+ }
+ else {
+   console.log("eh he's alright")
+ }
+
+ for (var i = 0; i < 10; i++) {
+   console.log('Current number is ' + 1)
+ }
+ 
+ //array position
+[item1, item2, item3]
+//in. 0 in. 1 in. 2 in. 3
+ // index
+ array[2] = item2 
+
+ //array with different types
+[
+  1, 
+  2.6, 
+  true, 
+  {firstName: 'Jace', lastName: 'Gold'}
+]
+
+ // array in array
+[
+  1,
+  2, 
+  'Jace'
+    [ 'item1',
+      'item2',
+      'item3'
+  ]
+]
+ // size of the array
+ arr.length
+
+ // add item on to the array
+ arr.push('henry left us!')
+
+ //remove the last element from the array
+ array.pop()
+
+
+
